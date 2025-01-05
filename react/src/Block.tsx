322fdbed86c824507ad252194c4b5f0b4e0c5ce7
Eric Lesslie <55772858+ericlesslie@users.noreply.github.com>
2025-01-04 23:23:11 -0500

    Update Block.tsx

--- react/src/Block.tsx
+++ react/src/Block.tsx
@@ -1,3 +1,14 @@
 export function Block(props) {
-    return <></>;
+    return   <div className="ce-block" data-id="r_C0D87V00">
+            <div className="ce-block__content">
+              <div
+                className="ce-paragraph cdx-block"
+                contenteditable="true"
+                data-placeholder-active="Type text or paste a link"
+                data-empty="false"
+              >
+                Test
+              </div>
+            </div>
+          </div>;
 }
